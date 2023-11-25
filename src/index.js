5b7aada612c612ba8498d367c62a1c9f4b3eab6b
jonas.marschner@mailbox.org <jonas.marschner@mailbox.org>
2023-11-25 15:09:17 +0100

    Build menu bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import "./style.css";
 
-console.log("Hello World!");
+const container = document.querySelector(".container");
+
+const menubar = (function () {
+    const container = document.createElement("nav");
+
+    const createMenubarItem = (text) => {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        li.appendChild(a);
+
+        a.textContent = text;
+        a.setAttribute("href", "#")
+
+        return li;
+    };
+
+    (function buildMenubar() {
+        container.appendChild(document.createElement("ul"))
+
+        const homeAnchor = createMenubarItem("Home");
+        const menuAnchor = createMenubarItem("Menu");
+        const contactAnchor = createMenubarItem("Contact");
+
+        const anchors = [homeAnchor, menuAnchor, contactAnchor];
+
+        
+        anchors.forEach(anchor => container.firstChild.appendChild(anchor));
+    })();
+
+    return { container };
+
+})();
+
+const content = (function (){
+    container = document.createElement("div");
+
+    return { container };
+})();
+
+container.appendChild(menubar.container);
+container.appendChild(content.container);
